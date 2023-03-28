e4ba8cca2e75e9e1656300cbab8531498586e8df
Agustin <agusti_cintassoriano@iescarlesvallbona.cat>
2023-03-28 20:32:20 +0200

    test

--- app/src/main/java/com/example/justmeet/MainActivity.kt
+++ app/src/main/java/com/example/justmeet/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
+        print("hola")
     }
 }
