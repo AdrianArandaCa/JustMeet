ce9f0f56e62985adfe2fd50e7e2722030c4a32e7
AdrianArandaCa <aacampanario91@gmail.com>
2023-04-18 16:34:11 +0200

    websocket

--- app/src/main/java/com/example/justmeet/Fragments/PlayFragment.kt
+++ app/src/main/java/com/example/justmeet/Fragments/PlayFragment.kt
@@ -9,10 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.justmeet.Activitys.GameActivity
 import com.example.justmeet.Models.*
+import com.example.justmeet.Socket.MessageListener
 import com.example.justmeet.Socket.Socket
+import com.example.justmeet.Socket.WebSocketManager
 import com.example.justmeet.databinding.FragmentPlayBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 import kotlin.coroutines.CoroutineContext
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,13 +27,9 @@ private lateinit var binding: FragmentPlayBinding
 private lateinit var webSocket: WebSocket
 var job = Job()
 
-class PlayFragment : Fragment(), CoroutineScope {
-    // TODO: Rename and change types of parameters
-
-
+class PlayFragment : Fragment(), MessageListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -37,75 +38,69 @@ class PlayFragment : Fragment(), CoroutineScope {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentPlayBinding.inflate(inflater, container, false)
+        binding.btnJugar.visibility = View.GONE
+        val serverUrl = "ws://172.16.24.123:45456/ws/${userLog.idUser}"
+        WebSocketManager.init(serverUrl, this)
         binding.btnBuscarPartida.setOnClickListener {
-            var socket = Socket(userLog)
-            val client = OkHttpClient()
-            val request = Request.Builder().url(socket.urlServer + userLog.idUser).build()
-            client.newWebSocket(request, socket)
-
-            Log.i("question",listQuestion[0].question1.toString())
-
+            thread {
+                kotlin.run {
+                    WebSocketManager.connect()
+                }
             }
-
-
-            // Log.i("question",listQuestion[0].question1.toString())
-
-
-              //val intento = Intent(context, GameActivity::class.java)
-               //startActivity(intento)
-
-
+            binding.btnJugar.visibility = View.VISIBLE
+        }
+        binding.btnJugar.setOnClickListener {
+            if ( WebSocketManager .sendMessage( " Client send " )) {
+                val intent = Intent(requireContext(), GameActivity::class.java)
+                startActivity(intent)
+            //addText( " Send from the client \n " )
+            }
+        }
+        /*closeConnectionBtn.setOnClickListener {
+            WebSocketManager.close()
+        }*/
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val client = OkHttpClient()
-//
-//        val request = Request.Builder().url("ws://172.16.24.123:45456:6666/ws/2").build()
-//
-//        webSocket = client.newWebSocket(request, object : WebSocketListener() {
-//
-//            override fun onOpen(webSocket: WebSocket, response: Response) {
-//                super.onOpen(webSocket, response)
-//
-//            }
-//
-//            override fun onMessage(webSocket: WebSocket, text: String) {
-//                super.onMessage(webSocket, text)
-//                val gson = Gson()
-//                try {
-//                    val data = gson.fromJson(text, User::class.java)
-//
-//
-//                    activity?.runOnUiThread {
-//                        binding.txtPruebaSocket.setText("Valor 1: ${data.name}\nValor 2: ${data.password}")
-//                    }
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                }
-//            }
-//            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
-//                super.onClosing(webSocket, code, reason)
-//
-//            }
-//
-//            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-//                super.onFailure(webSocket, t, response)
-//
-//            }
-//        })
     }
 
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
+    override fun onConnectSuccess() {
+        addText( " Connected successfully \n " )
+    }
 
+    override fun onConnectFailed() {
+        addText( " Connection failed \n " )
+    }
 
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        webSocket.close(1000, null)
-//    }
+    override fun onClose() {
+        addText( " Closed successfully \n " )
+    }
+
+    override fun onMessage(text: String?) {
+        val gson = Gson()
+        val listType = object : TypeToken<ArrayList<Question>>() {}.type
+        if (text != null) {
+            if(text.startsWith("-", 1,false)){
+                addText( " Receive message: $text \n " )
+            } else {
+                listQuestion = gson.fromJson(text, listType)
+            }
+        }
+    }
+
+    private fun addText(text: String?) {
+        activity?.runOnUiThread {
+            //contentEt.text.append(text)
+        }
+    }
+
+    override fun onDestroy() {
+        super .onDestroy ()
+        WebSocketManager.close()
+    }
+
 }
 
 
